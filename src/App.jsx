fd6d061bc5e5b925304da30a1a6f9ebb8aa2c328
MdGulam <ghassnain396@gmail.com>
2023-07-16 13:48:42 +0530

    Update by Gulam

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import Calculator from "./component/Calculator";
-// import Header from "./component2/Header";
-
 
 export default function App() {
   return (
     <div>
-      {/* <Header name="Rahul"/>
-      <p>Rahul is a Good Boy</p>
-      <Header name="Anuj"/>
-      <Header name="Vivek"/> */}
-     
       <Calculator />
     </div>
   )
